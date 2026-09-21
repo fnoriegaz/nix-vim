1a4ce06a65511466625bee79ea359b8434e0a2b3
fnoriegaz <50144703+fnoriegaz@users.noreply.github.com>
2026-09-21 21:00:22 +0200

    Refactor tokyo night theme setup in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,15 +94,15 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
 
-vim.pack.add { gh 'folke/tokyonight.nvim' }
+cmd.packadd('folke/tokyonight.nvim')
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
-  }
+  --require('tokyonight').setup {
+   -- styles = {
+   --   comments = { italic = false }, -- Disable italics in comments
+   -- },
+  --}
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+cmd.colorscheme 'tokyonight-night'
